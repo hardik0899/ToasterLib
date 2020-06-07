fd42e16b958123d66c7d329b652e95df5ef62cee
Hardik Dua <hardikdua08@gmail.com>
2020-06-07 14:05:09 +0530

    Sample app made using the library

--- app/src/main/java/hardik/lib/toasterlib/MainActivity.java
+++ app/src/main/java/hardik/lib/toasterlib/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import hardik.lib.toasterlibrary.ToasterMessage;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToasterMessage.showToast(MainActivity.this, "Yeah, here we go :) We are able to generate a toast using Hardik's Toast-Lib library");
     }
 }
